6a5a03dcbda694578a29e80babc76114eec07c5b
victor_mikh <victor_mikh@grandejob.com>
2015-10-05 19:27:04 +0300

    Добавил возможность открывать меню для редактирования страницы с загрузкой данных таблицы

--- dolibarr/htdocs/DBManager/dbBuilder.php
+++ dolibarr/htdocs/DBManager/dbBuilder.php
@@ -5,13 +5,32 @@
  * Date: 30.09.2015
  * Time: 17:36
  */
+
 class dbBuilder{
+    
     public $mysqli;
     public function __construct()
     {
         include 'db.php';
         $db = new dbMysqli();
         $this->mysqli = $db->mysqli;
+    }
+    function fBuildEditForm($title, $field, $theme, $tablename){
+        $edit_form ="<tr>
+                        <td>".$title."</td>";
+        $edit_form.="<td>";
+        if($field->type != 16){
+            if($field->length<=50)
+                $edit_form.='<input id = edit_'.$field->name.' class="text" maxlength="45" name="label" type="text" value="">';
+            else
+                $edit_form.='<textarea id = edit_'.$field->name.' class="text" name="description"></textarea>';
+        }else{
+            $field_name = "'".$field->name."'";
+            $edit_form.='<img id="edit_' .$field->name . '" src="' . DOL_URL_ROOT . '/theme/' . $theme . '/img/switch_on.png" onclick="change_switch(0, ' . $tablename . ', ' . $field_name . ');">';
+        }
+        $edit_form.="</td>
+                    </tr>";
+        return $edit_form;
     }
     public function fShowTable($title = array(), $sql, $tablename, $theme){
         $result = $this->mysqli->query($sql);
@@ -42,10 +61,17 @@ class dbBuilder{
 
 
         $count = 0;
+        $create_edit_form = false;
+        $edit_form = "<a href='#x' class='overlay' id='editor'></a>
+                     <div class='popup'>
+                     <form>
+                     <input type='hidden' id='rowid' name='rowid' value='0'>
+                     <table>
+                     <tbody>";
         while($row = $result->fetch_assoc()) {
             $count++;
             $class = fmod($count,2)==1?("impair"):("pair");
-            $table .= "<tr id = $count class='".$class."'>\r\n";
+            $table .= "<tr id = ".$row['rowid']." class='".$class."'>\r\n";
 //            $table .= "<tr id = $count class=".fmod($count,2)==1?('impair'):('pair').">\r\n";
             $id = $row['rowid'];
 //            $table .= '<td >'.$class.'</td>';
@@ -58,41 +84,85 @@ class dbBuilder{
 
                 $col_name = "'".$fields[$num_col]->name."'";
                 if($cell != 'rowid') {
+                    if(!$create_edit_form)//Формирую форму для редактирования
+                        $edit_form.=$this->fBuildEditForm($title[$num_col-1]['title'], $fields[$num_col], $theme, $tablename);
                     if($fields[$num_col]->type == 16){
                         if($value == '1') {
-                            $table .= '<td><img id="' . $row['rowid'] . $fields[$num_col]->name . '" src="' . DOL_URL_ROOT . '/theme/' . $theme . '/img/switch_on.png" onclick="change_switch(' . $row['rowid'] . ', ' . $tablename . ', ' . $col_name . ');"> </td>';
+                            $table .= '<td id="' . $row['rowid'] . $fields[$num_col]->name . '"><img id="img' . $row['rowid'] . $fields[$num_col]->name . '" src="' . DOL_URL_ROOT . '/theme/' . $theme . '/img/switch_on.png" onclick="change_switch(' . $row['rowid'] . ', ' . $tablename . ', ' . $col_name . ');"> </td>';
                         }else{
-                            $table .= '<td><img id="' . $row['rowid'] . $fields[$num_col]->name . '" src="' . DOL_URL_ROOT . '/theme/' . $theme . '/img/switch_off.png" onclick="change_switch(' . $row['rowid'] . ', ' . $tablename . ', ' . $col_name . ');"> </td>';
+                            $table .= '<td id="' . $row['rowid'] . $fields[$num_col]->name . '"><img id="img' . $row['rowid'] . $fields[$num_col]->name . '" src="' . DOL_URL_ROOT . '/theme/' . $theme . '/img/switch_off.png" onclick="change_switch(' . $row['rowid'] . ', ' . $tablename . ', ' . $col_name . ');"> </td>';
                         }
                     }
                     else
-                        $table .= '<td>' . $value . '</td>';
+                        $table .= '<td id="' . $row['rowid'] . $fields[$num_col]->name . '">' . $value . '</td>';
 
 
 
                 }
                 $num_col++;
             }
+            if(!$create_edit_form) {
+                $create_edit_form = true;
+                $edit_form .="    </table>
+                               </form>
+                            <a class='close' title='Закрыть' href='#close'></a>
+                            </br>
+                            <button onclick='edit_item();'>Сохранить</button>
+                            <button onclick='close_form();'>Закрыть</button>
+                            </div>
+                             ";
+//                var_dump($edit_form);
+//                die('tt');
+            }
+//
             $table .= '<td style="width: 20px" align="left">
-                            <button style="width: 35px" title = "Редактировать" onclick="edit_item('.$row['rowid'].', '.$tablename.');">
-                                <img src="' . DOL_URL_ROOT . '/theme/' . $theme . '/img/edit.png" alt="Редактировать" style="vertical-align: middle">
-                            </button>
+
+                <img  src="' . DOL_URL_ROOT . '/theme/' . $theme . '/img/edit.png" title="Редактировать" style="vertical-align: middle" onclick="edit_item('.$row['rowid'].');">
+
 
                        </td>';
             $table .= '</tr>';
         }
         $table .= '</tbody>'."\r\n";
-        $table .= '</table>';
+        $table .= '</table>'."\r\n";
+        $table .= '</form>'."\r\n";
+
+        $table .= $edit_form;
         $table .= "<script>
+                    function load_info(){
+                        console.log('test');
+                    }
                     function del_confirm(rowid, tablename) {
                         if (confirm('Удалить пункт меню?'))
                             location.href = 'http://".$_SERVER["SERVER_NAME"]."/dolibarr/htdocs/DBManager/dbManager.php?rowid='+rowid+'&del=1&tablename='+tablename;
                     }
-                    function edit_item(rowid, tablename){
-                        location.href ='http://".$_SERVER["SERVER_NAME"]."/dolibarr/htdocs/DBManager/dbManager.php?rowid='+rowid+'&edit=1&tablename='+tablename;
+                    function close_form(){
+                        location.href = '#close';
+                    }
+                    function edit_item(rowid){
+                        var sID=rowid.toString();
+                        var tr = document.getElementById(rowid);
+                        var elements = tr.getElementsByTagName('td');
+                        for(var i=0; i<elements.length; i++){
+                            if(elements[i].id.substr(0, sID.length) == sID){
+                                var fieldname = elements[i].id.substr(sID.length);
+                                var edit_field = document.getElementById('edit_'+fieldname);
+                                var source_field = document.getElementById(sID+fieldname);
+                                var img = source_field.getElementsByTagName('img');
+                                if(img.length == 1){
+                                    edit_field.src = img[0].src;
+                                }else
+                                    edit_field.value = source_field.innerHTML;
+                            }
+                        }
+                        location.href = '#editor';
                     }
                     function change_switch(rowid, tablename, col_name){
-                        var x = document.getElementById(rowid+col_name);
+                        var x;
+                        if(rowid != 0)
+                            x = document.getElementById('img'+rowid+col_name);
+                        else
+                            x = document.getElementById('#edit_'+col_name);
                         var check = false;
                         if(x.src == 'http://".$_SERVER["SERVER_NAME"].DOL_URL_ROOT."/theme/".$theme."/img/switch_on.png'){
                             x.src = 'http://".$_SERVER["SERVER_NAME"].DOL_URL_ROOT."/theme/".$theme."/img/switch_off.png';
@@ -100,7 +170,8 @@ class dbBuilder{
                             x.src = 'http://".$_SERVER["SERVER_NAME"].DOL_URL_ROOT."/theme/".$theme."/img/switch_on.png';
                             check = true;
                         }
-                        update_data(rowid, tablename, col_name, check);
+                        if(rowid != 0)
+                            update_data(rowid, tablename, col_name, check);
                     }
                     function update_data(rowid, tablename, col_name, check){
                         $.ajax({
